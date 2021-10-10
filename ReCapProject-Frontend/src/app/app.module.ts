5be9fd0ade6cd969cbde865d0c7eee0c4450f69a
alikemalcelik <alikemalcelik42@gmail.com>
2021-10-11 00:08:44 +0300

    first commit

---
+++ ReCapProject-Frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CarComponent } from './components/car/car.component';
+import { CarService } from './services/car.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NaviComponent,
+    BrandComponent,
+    CarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
